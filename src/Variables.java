50c3e805caa2cc89567d9c4a2182ae237c54c920
hphung9492 <106184699+hphung9492@users.noreply.github.com>
2022-05-26 14:39:18 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -15,6 +15,23 @@ public class Variables
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println("Byte is an 8-bit signed two's complement integer ranging from -128 to 127");
+        System.out.println("Byte data type can be used in large arrays to save memory or they can also be used in place of int");
+        System.out.println("Short is a 16-bit signed two's complement integer ranging from -32768 to 32767");
+        System.out.println("Like byte, short data type can be used in large arrays to save memory");
+        System.out.println("Int is a 32-bit signed two's complement integer, ranging from -2^31 to 2^32-1");
+        System.out.println("In Java SE 8 or later, int can be used to represent an unsigned 32-bit integer");
+        System.out.println("Long is a 64-bit two's complement integer ranging from -2^63 to 2^64-1");
+        System.out.println("Long data type can be used to represent an unsigned 64-bit long");
+        System.out.println("Float is a single-precision 32-bit IEEE 754 floating point");
+        System.out.println("Float can be used in large arrays to save memory but should never be used for precise values");
+        System.out.println("Double is a double-precision 64-bit IEEE 754 floating point");
+        System.out.println("Double is a default choice for decimal values but never for precise values");
+        System.out.println("Boolean has only two possible values: true and false");
+        System.out.println("Boolean can be use for simple flags that track true/false condition");
+        System.out.println("Char is a single 16-bit Unicode character");
+        System.out.println("Char has a minimum value of 0 and a maximum value of 65535");
+        
     }
 
 }
